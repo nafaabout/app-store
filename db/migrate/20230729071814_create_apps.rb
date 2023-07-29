16d67e6c3a048d5e05699cd44a031f8662b44a37
Nafaa Boutefer <nafaabout@gmail.com>
2023-07-29 08:19:20 +0100

    First punch

---
+++ db/migrate/20230729071814_create_apps.rb
@@ -0,0 +1,13 @@
+class CreateApps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :apps do |t|
+      t.string :name
+      t.text :description
+      t.string :version
+      t.decimal :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
