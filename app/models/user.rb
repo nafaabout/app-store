0b3cc89303d5bdd83804135352ab3872ff2265ec
Nafaa Boutefer <nafaabout@gmail.com>
2023-07-29 09:10:38 +0100

    Add free_apps and paid_apps methods to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
-  has_many :apps
-  has_many :reviews
+  has_many :app_downloads, dependent: :destroy
+  has_many :apps, through: :app_downloads
+
+  def free_apps
+    apps.where(price: nil)
+  end
+
+  def paid_apps
+    apps.where.not(price: nil)
+  end
+
 end
