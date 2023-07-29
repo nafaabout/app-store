49ae00ed950cef22921f3ed2afa9665c88efcca6
Nafaa Boutefer <nafaabout@gmail.com>
2023-07-29 10:56:46 +0100

    Mount AppDiag::Engine at /app_diag

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  mount AppDiag::Engine => '/app_diag'
+
   resources :reviews
   resources :categories
   resources :apps
   resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
