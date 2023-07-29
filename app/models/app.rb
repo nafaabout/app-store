4b876eebef6956f5b133945c2b3c06ab6e157c07
Nafaa Boutefer <nafaabout@gmail.com>
2023-07-29 10:01:48 +0100

    Fix typo

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,6 +1,6 @@
 class App < ApplicationRecord
   belongs_to :user
-  has_many :reviewers, class: 'User', through: :reviews
+  has_many :reviewers, class_name: 'User', through: :reviews
   has_many :reviews
   has_many :app_categories
   has_many :categories, through: :app_categories
